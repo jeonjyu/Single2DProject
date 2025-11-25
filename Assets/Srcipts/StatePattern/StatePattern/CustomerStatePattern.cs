6c2f9d90e1625dafe81bcb27963a934000af181f
jeonjyu <jihoojeon77@gmail.com>
2025-11-26 03:07:36 +0900

    [Add] CustomerStatePattern
    - CustomerStatePattern에 행동 로직 추가

--- Assets/Srcipts/StatePattern/StatePattern/CustomerStatePattern.cs
+++ Assets/Srcipts/StatePattern/StatePattern/CustomerStatePattern.cs
@@ -6,13 +6,18 @@ public class CustomerStatePattern : MonoBehaviour
     [SerializeField] private Queue<GameObject> _waitingQueue;
     private ICharacterState _currentState;
     private bool _isCheckingout;
-
+    private int _itemIdx;
+    private Item _item;
     public bool IsChecking { get => _isCheckingout; set => _isCheckingout = value; }
+    public int ItemIdx { get => _itemIdx; set => _itemIdx = value; }
 
     public ICharacterState _shoppingState;
     public ICharacterState _idleState;
     public ICharacterState _checkoutState;
     public ICharacterState _exitState;
+
+    System.Random rd = new System.Random();
+
 
     private void Start()
     {
@@ -22,9 +27,10 @@ public class CustomerStatePattern : MonoBehaviour
         _exitState = new ExitState(transform);
 
         Debug.Log($"[CustomerStatePattern] {gameObject}");
+        SetShoppingList();
         SetState(_shoppingState);
         
-        //GetInLine();
+        GetInLine();
     }
 
     public void SetState(ICharacterState newState)
@@ -39,23 +45,38 @@ public class CustomerStatePattern : MonoBehaviour
 
     void Update()
     {
-        timer += Time.deltaTime; // 프레임마다 시간을 더함
-        if (timer >= delayTime)
-        {
-            GetInLine();
-        }
+        //timer += Time.deltaTime; // 프레임마다 시간을 더함
+        //if (timer >= delayTime)
+        //{
+        //    //GetInLine();
+        //}
+    }
+
+    public void SetShoppingList()
+    {
+        // 값이 안변함
+        // 호출 위치가 잘못 된 듯
+        _itemIdx = Random.Range(1, ItemStockManager.Instance.Count + 1);
+        Debug.Log($"[CustomerStatePattern] {ItemStockManager.Instance.GetItem(_itemIdx)}");
+    }
+
+    public void MoveToTarget()
+    {
+        // _itemIdx로 정해진 대상의 오브젝트 위치를 연결해준다 (어떻게?)
+        // 암튼 타겟이 정해지면 이동
     }
 
     public void GetInLine()
     {
         WaitingQueueManager.Instance.EnqueueCustomer(gameObject);
-        //Debug.Log("[CustomerStatePattern] 대기");
         SetState(_idleState);
         // 계산 끝날 때까지 기다리는 로직 추가
-        // 점원이 왜 동작을 안하지
+        // 기다리는 시간 추가(item Count에 초 곱해서 기다리기)
+        _item = ItemStockManager.Instance.GetItem(_itemIdx);
+        StoreBalanceManager.Instance.AddBalnce(_item.Price);
+        Debug.Log("[CustomerStatePattern] 계산 끝");
         ExitStore();
     }
-
 
 
     public void ExitStore()
